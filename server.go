d177c225981049bf6f91d679f0cca91be06ad2bb
jb <101614021+jbc2313@users.noreply.github.com>
2023-08-22 23:37:48 -0500

    added a header to select pages

--- server.go
+++ server.go
@@ -18,20 +18,29 @@ type PageDetail struct {
 func greet(w http.ResponseWriter, r *http.Request) {
 	data := PageDetail {
 		Title: "GoSite",
-		Body: []string{"This site is running on Go","It is 10x faster then Node.js"},
+		Body: []string{"This site is running on Go","It is generally faster then Node.js","Go has concurrency, allowing multiple tasks at once to take place."},
 		Footer: "Golang",
 	}
 	tmpl.Execute(w, data)
 }
 
+func about(w http.ResponseWriter, r *http.Request) {
+	data := PageDetail {
+		Title: "About",
+		Body: []string{"This site is built with only the go std lib"},
+		Footer: "Golang",
+	}
+	tmpl.Execute(w, data)
+};
 
 func main() {
 	mux := http.NewServeMux()
-	tmpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+	tmpl = template.Must(template.ParseFiles("templates/index.gohtml", "templates/header.gohtml"))
 
 	fs := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 	mux.HandleFunc("/", greet)
+	mux.HandleFunc("/about", about)
 
 	log.Fatal(http.ListenAndServe(":8090", mux))
 
