bf7d1181de02be4523ff26dae0f59d6907bbdf7e
jb <101614021+jbc2313@users.noreply.github.com>
2023-08-22 22:40:00 -0500

    very basic site is working

--- server.go
+++ server.go
@@ -1,7 +1,7 @@
-package server
+package main
 
 import (
-	"fmt"
+	//"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,15 +9,30 @@ import (
 
 var tmpl *template.Template
 
+type PageDetail struct {
+	Title string
+	Body []string
+	Footer string
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	data := PageDetail {
+		Title: "GoSite",
+		Body: []string{"This site is running on Go","It is 10x faster then Node.js"},
+		Footer: "Golang",
+	}
+	tmpl.Execute(w, data)
+}
 
 
+func main() {
+	mux := http.NewServeMux()
+	tmpl = template.Must(template.ParseFiles("templates/index.gohtml"))
 
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.HandleFunc("/", greet)
 
-
-
-
-func server() {
-
-
+	log.Fatal(http.ListenAndServe(":8090", mux))
 
 }
